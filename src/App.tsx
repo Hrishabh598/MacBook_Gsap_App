daf1482980738c3c20704a2a145058377d100504
hrisabh.tiwari <hrishabh.tiwari@globallogic.com>
2026-04-18 17:07:10 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from './components/NavBar'
+
+const App = () => {
+    return (
+        <main>
+        <Navbar />
+        <h1 className="text-3xl font-bold underline text-indigo-700">Hello World</h1>
+        </main>
+    )
+}
+export default App
---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+    {label:'Store'},
+    {label:'Mac'},
+    {label:'iPhone'},
+    {label:'Vision'},
+    {label:'Watch'},
+    {label:'AirPods'},
+];
